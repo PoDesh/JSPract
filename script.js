005e0b8b2d74f11b30002ca50fc4dbb95bb92ca6
PoDesh <166140955+PoDesh@users.noreply.github.com>
2024-04-10 16:27:03 +0300

    third

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 /*var - существует везде
 let - существует только тогда, когда код до нее доходит*/
 
-alert('Hello World');
-prompt('hey');
+/*alert('Hello World');
+prompt('hey');*/
 
 // +prompt - строку в число
 // let incr = 10, decr = 26 - в переменной можно перечислять через запятую
@@ -23,4 +23,48 @@ prompt('hey');
 
 // ! - оператор отрицания. Который обращает значение в обратное - правду в ложь и наоборот
 
-// ы
+/*'use strict';
+console.log(3 + 4 * 5); // 3 + 20*/
+// Expected output: 23
+
+//console.log(4 * 3 ** 2); // 4 * 9
+// Expected output: 36
+
+/*let a;
+
+
+<script src="script.js" t></script>
+alert(1)
+
+function pow(x, n) { }
+var result = 1;
+for (var i = 0: i < n; i++) { }
+
+return result;
+var x = promt("x? , "");
+var n = promt("n?", "");
+
+if (n < 0) {] }*/
+
+
+
+let BudgetMonth = prompt("Ваш бюджет на месяц?", ''),
+time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let AppData = {
+    budget: BudgetMonth,
+    datetime: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную  статью расходов в этом месяце", ''),
+    a4 = prompt("Во солько обойдется?", '');
+
+AppData.expenses.a1 = a2;
+AppData.expenses.a3 = a4;
+alert(AppData.budget / 30);
